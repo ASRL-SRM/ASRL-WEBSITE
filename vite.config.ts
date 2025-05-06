795f48f8de8f01629afb95334ac8c65b16a79f32
Akinc5 <akshansh9816264920@gmail.com>
2025-05-07 01:04:42 +0530

    Move vite to dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'asrlwebsiteasrl.in-2025aimhighflyhigher', // 🔁 Replace with your repo name if it's different
+  base: '/', // 🔁 Replace with your repo name if it's different
   plugins: [react()],
 })
